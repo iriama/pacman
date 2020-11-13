f7505da596f5838b5ef6d3ed132c6661912c2dd1
iriama <iriama@protonmail.com>
2020-11-13 15:29:18 +0100

    cleanup

--- Equipe_27_Sprint_1/src/rendering/IBuild.java
+++
@@ -1,5 +0,0 @@
-package rendering;
-
-public interface IBuild {
-    void build();
-}
--- Equipe_27_Sprint_1/src/rendering/IRenderEngine.java
+++
@@ -1,9 +0,0 @@
-package rendering;
-
-import java.awt.*;
-
-public interface IRenderEngine {
-    void draw(Graphics2D g);
-
-    void addEntity(Entity entity);
-}
